032f0553eb7df000a7d39ce1f879a79fa8cb7e8c
Umair Yahya <130144106+muhammadnomansiddiqui@users.noreply.github.com>
2023-04-07 19:46:10 +0500

    My first assignment

---
+++ app.js
@@ -0,0 +1,110 @@
+// // Qno :1
+
+// let userFirst =+prompt("Enter a First Number")
+// let userSecond =+prompt("Enter a Second Number")
+
+// if(userFirst > userSecond){
+//     console.log(userFirst)
+// }
+// else{
+//     console.log(userSecond)
+// }
+
+// //  Qno :2
+// let userFirst =+prompt("Enter a Specified Sign")
+
+// let num1=-0
+// if( userFirst > num1){
+//     alert("This Sign is +"+userFirst)
+// }
+// else{
+//     alert("This Sign is "+userFirst)
+// }
+// //QNO:3
+//  let num1 = +(prompt("Enter the first number:"));
+// let num2 = +(prompt("Enter the second number:"));
+// let num3 = +(prompt("Enter the third number:"));
+// let num4 = +(prompt("Enter the fourth number:"));
+// let num5 = +(prompt("Enter the fifth number:"));
+
+// let largest = num1;
+
+// if (num2 > largest) {
+//  largest = num2;
+// }
+
+// if (num3 > largest) {
+//   largest = num3;
+// }
+
+// if (num4 > largest) {
+//   largest = num4;
+// }
+
+// if (num5 > largest) {
+//   largest = num5;
+// }
+
+// console.log("The largest number is " + largest);
+// // Qno :4
+
+// for(let i= 0;i <= 15;i++){
+
+// if(i%2==0){
+//     console.log(i+" even");
+// }
+// else{
+//     console.log(i+" Odd");
+// }
+// }
+
+// // Qno :5
+
+// let   totalNum= prompt("Enter Total Number");
+
+// if(totalNum>=90 && totalNum<=100){
+//     console.log("Grade : A")
+// }
+// else if(totalNum>=80 && totalNum<90){
+//     console.log("Grade : B")
+// }
+// else if(totalNum>=70 && totalNum<80){
+//     console.log("Grade : C")
+// }
+// else if(totalNum>=60 && totalNum<70){
+
+//     console.log("Grade : D")
+// }
+// else if(totalNum<=60){
+//     console.log("Grade : F")
+// }
+// // QNO:06
+// for(var i=1; i<=100; i++){
+//     if(i % 3 == 0 && i % 5 == 0)
+//     {
+//         console.log("FIZZBUZZ");
+//     }
+//     else if(i % 3 ==0)
+//     {
+//         console.log("FIZZ");
+//     }
+//     else if(i % 5==0)
+//     {
+//         console.log("BUZZ");
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+// // QNO:07
+// let n=5;
+// let right =""
+// for(let i=1 ;i<=n;i++)
+// {
+//     for(let j=1 ; j<=i ; j++)
+//     {
+//            right += "*";
+//     }
+//    right += "\n";
+// }
+// console.log(right);
